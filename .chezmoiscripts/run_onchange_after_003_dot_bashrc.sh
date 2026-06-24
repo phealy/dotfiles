85127de12e6c65dccbca8dab85e87a78e4a2d2e6
Patrick W. Healy <patrick.healy@microsoft.com>
2026-06-24 11:54:01 -0500

    add uv and fix systemd environment stuff

--- .chezmoiscripts/run_onchange_after_003_dot_bashrc.sh
+++ .chezmoiscripts/run_onchange_after_003_dot_bashrc.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
 bash --login --noprofile --norc -t -c '. ~/.commonrc; export -p' | sed -e 's/declare -x //' | grep -Ee '^ASDF' -e '^GO' -e '^PATH' > $HOME/.config/environment.d/10-path.conf
+systemctl --user daemon-reload
