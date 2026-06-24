15f35e34c61fc8eb389668cdb69c41640a091460
Patrick W. Healy <patrick.healy@microsoft.com>
2026-06-24 11:42:49 -0500

    add systemd path environment vars

---
+++ .chezmoiscripts/run_onchange_after_003_dot_bashrc.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+bash --login --noprofile --norc -t -c '. ~/.commonrc; export -p' | sed -e 's/declare -x //' | grep -Ee '^ASDF' -e '^GO' -e '^PATH' > $HOME/.config/environment.d/10-path.conf
